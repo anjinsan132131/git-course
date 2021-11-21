381e6ef76aa421c94a97568d5bf11c1f175c8321
anjinsan132131 <anjinsan132131@gmail.com>
2021-11-21 14:53:32 +0300

    test32

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-document.body.innerHTML = '<h1> hello from Javascript </h1>'  
-console.log('test2')
+document.body.innerHTML = '<h1> hello from Javascript </h1>'; 
+console.log('test2');
+console.log('test32');
